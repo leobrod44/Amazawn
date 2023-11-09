fc28f9dfad0fd55e271f4f78c2b3790893c2ac2f
kara <racha.kara@gmail.com>
2023-11-09 12:34:03 -0500

    Update #2 for Tracking page
    - started the containers creation and styling (still needs editing)

--- Frontend/amazawn/src/pages/TrackingPage.jsx
+++ Frontend/amazawn/src/pages/TrackingPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "../components/Logo";
+import "../styling/index.css";
 
 const TrackingPage = () => {
 
@@ -19,28 +20,46 @@ const TrackingPage = () => {
         }
     }; */
 
-
     return(
         <div>
-            <div style={{ textAlign: "center" }}>
-                <Logo style={{ margin: "0 auto" }} />
-            </div>
-            <h3 style={{ textAlign: "center" }}>Track you Package</h3>
-
-
-            <label>
-                Enter Delivery ID:
-                <input type="text" />
-            </label>
-            <button >Track Delivery</button>
-
-
+            <div style={{ textAlign: "center" }} className="trackdelivery">
                 <div>
-                <h2>Delivery Information</h2>
-                <p>Status: {}</p>
-                <p>Estimated Delivery Time: {}</p>
-                {/* Add more information based on your API response */}
+                    <Logo style={{ margin: "0 auto" }} />
                 </div>
+                <h3 style={{ marginBottom: "3rem"}}>Track you Package</h3>
+
+                <label style={{ marginBottom: "0.3rem",  marginTop: "2rem", color:"#024f35"}}>Enter you delivery ID: </label>
+                <input type="text" className="form-input"/>
+                <div>
+                    <button
+                    type="button"
+                    className="btn"
+                    onClick={() => {}}
+                    style={{ marginTop: "1.5rem" }}
+                    >
+                    Track Delivery
+                    </button>
+                </div>
+            </div>
+            
+            <div style={{ textAlign: "center" }} className="displaytrackinfo">
+                <div>
+                    <Logo style={{ margin: "0 auto" }} />
+                </div>
+                <h3 style={{ marginBottom: "3rem"}}>Track you Package</h3>
+                <div style={{ textAlign: "left" }} className="deliveryinfo">
+                    <h4>Delivery Information</h4>
+                    <p>Status: {}</p>
+                    <p>Estimated Delivery Time: {}</p>
+                    <p>Delivery Address: {}</p>
+                    <p>Customer support contact: {}</p>
+
+                    <label>PROGRESSION BAR</label>
+                    <progress max="1" value={0.5} className="progressbar">
+                        <div></div>
+                    </progress>
+                </div>
+            </div>
 
         </div>
     );
