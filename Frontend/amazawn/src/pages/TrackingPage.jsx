6bc192f231dd2c169a1bc38fa98158a9f3f4d1a8
kara <racha.kara@gmail.com>
2023-11-29 14:53:02 -0500

    Update of Tracking Page
    - fetching delivery info linked to inputed ID from backend (still a draft)

--- Frontend/amazawn/src/pages/TrackingPage.jsx
+++ Frontend/amazawn/src/pages/TrackingPage.jsx
@@ -1,18 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../components/Logo";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ProgressBar from "../components/ProgressBar";
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styling/Tracking.css";
 
 const TrackingPage = () => {
+
+    const [showTracking, setShowTracking] = useState(true);
+    const [trackingId, setTrackingId] = useState('');
+    const [trackingInfo, setTrackingInfo] = useState({
+        progress: 0,
+        eta: null,
+        lastMilestone: null,
+      });
+
+    const toggleButton = async () => {
+        // Get the current date
+        const currentDate = new Date();
     
-    const [showTracking, setshowTracking] = useState(true);
-    const toggleButton = () => {
-        setshowTracking(!showTracking);
-    }
+        //Data to send to the backend
+        const requestData = {
+          trackingId: trackingId,
+          currentDate: currentDate.toISOString(), // Convert to ISO string format
+        };
+
+        try {
+            // Make a POST request to the backend with the tracking ID and current date
+            const response = await fetch('your_backend_api_endpoint', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(requestData),
+            });
+      
+            // Handle the response from the backend (you can customize this part based on your backend)
+            if (response.ok) {
+                const responseData = await response.json();
+                // Update the state with the received tracking information
+                setTrackingInfo({
+                    progress: responseData.progress,
+                    eta: new Date(responseData.eta),
+                    lastMilestone: new Date(responseData.lastMilestone),
+                });
+
+                // Toggle the tracking display
+                setShowTracking(!showTracking);
+            } else {
+              // Handle errors or display a message to the user
+              //console.error('Error sending tracking request to the backend');
+            }
+          } catch (error) {
+            // Handle network errors or other exceptions
+            console.error('Error:', error);
+          }
+    };
 
     return(
         <div>
@@ -25,7 +69,13 @@ const TrackingPage = () => {
                 <h3 style={{ marginBottom: "3rem"}}>Track your Package</h3>
 
                 <label style={{ marginBottom: "0.3rem",  marginTop: "2rem", color:"#024f35"}}>Enter your delivery ID: </label>
-                <div><input type="text" className="id-input"/></div>
+                <div><input 
+                        type="text" 
+                        className="id-input" 
+                        value={trackingId}
+                        onChange={(e) => setTrackingId(e.target.value)}
+                    />
+                </div>
                 <div>
                     <button
                     type="button"
@@ -53,10 +103,10 @@ const TrackingPage = () => {
                     <p style={{fontWeight: "bold"}}> Delivery ID :&nbsp;</p><p>12345{}</p>
                     </div>
                     <div style={{display: "flex", marginBottom: "0rem"}} className="form-label">
-                    <p style={{fontWeight: "bold"}}> Status :&nbsp;</p><p>In Transit{}</p>
+                    <p style={{fontWeight: "bold"}}> Status :&nbsp;</p><p>In Transit{/*{trackingInfo.progress}*/}</p>
                     </div>
                     <div style={{display: "flex", marginBottom: "0rem"}} className="form-label">
-                    <p style={{fontWeight: "bold"}}>Estimated Delivery Date :&nbsp;</p><p>Monday, November 20th, 2023{}</p>
+                    <p style={{fontWeight: "bold"}}>Estimated Delivery Date :&nbsp;</p><p>Monday, November 20th, 2023{/*{trackingInfo.eta.toLocaleString()}*/}</p>
                     </div>
                     <div style={{display: "flex", marginBottom: "0rem"}} className="form-label">
                     <p style={{fontWeight: "bold"}}>Delivery Address :&nbsp;</p><p> 1455, Blvd. De Maisonneuve Ouest, Montreal, H3G 1M8{}</p>
