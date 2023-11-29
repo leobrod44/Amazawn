a9504baac9e54a93e553534bb46781a5f83e174c
layanat <tounsilayana@gmail.com>
2023-11-29 16:49:17 -0500

    undid a bug i introduced

--- Frontend/amazawn/src/App.js
+++ Frontend/amazawn/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { BrowserRouter, Route, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomePage from './pages/HomePage';
 import RequestDelivery from'./pages/RequestDelivery';
