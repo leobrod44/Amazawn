07c9356f30f8fcfa562408e2d014b87595ba7d95
kara <racha.kara@gmail.com>
2023-11-30 13:52:25 -0500

    Tracking Page bug
    - divided the page on 2 pages
    - can't test it on my side

---
+++ Frontend/amazawn/src/components/TrackingDataPage.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "./Logo";
+import ProgressBar from "../components/ProgressBar";
+
+const TrackingDataPage = ({
+    dID,
+    prog,
+    eta,
+    progStatus
+}) => (
+        <div
+        style={{ textAlign: "center", display: "block" }}
+        className="displaytrackinfo"
+        id="track2"
+    >
+        <div>
+        <Logo style={{ margin: "0 auto" }} />
+        </div>
+        <h3 style={{ marginBottom: "3rem" }}>Track your Package</h3>
+        <div style={{ textAlign: "left" }} className="deliveryinfo">
+        <h4
+            style={{
+            marginBottom: "0.3rem",
+            marginTop: "2rem",
+            color: "#024f35",
+            }}
+        >
+            Delivery Information
+        </h4>
+
+        <div
+            style={{ display: "flex", margin: "0" }}
+            className="form-label"
+        >
+            <p style={{ fontWeight: "bold" }}> Delivery ID :&nbsp;</p>
+            <p>{dID}</p>
+        </div>
+        <div
+            style={{ display: "flex", marginBottom: "0rem" }}
+            className="form-label"
+        >
+            <p style={{ fontWeight: "bold" }}> Status :&nbsp;</p>
+            <p>{progStatus}</p>
+        </div>
+        <div
+            style={{ display: "flex", marginBottom: "0rem" }}
+            className="form-label"
+        >
+            <p style={{ fontWeight: "bold" }}>
+            Estimated Delivery Date :&nbsp;
+            </p>
+            <p>{eta && new Date(eta).toLocaleString()}</p>
+        </div>
+        <div
+            style={{ display: "flex", marginBottom: "0rem" }}
+            className="form-label"
+        >
+            <p style={{ fontWeight: "bold" }}>Delivery Address :&nbsp;</p>
+            <p> 1455, Blvd. De Maisonneuve Ouest, Montreal, H3G 1M8{}</p>
+        </div>
+        <div
+            style={{ display: "flex", marginBottom: "0rem" }}
+            className="form-label"
+        >
+            <p style={{ fontWeight: "bold" }}>
+            Customer support contact :&nbsp;
+            </p>
+            <p>customersupport@amazawn.com</p>
+        </div>
+
+        <ProgressBar progression={prog / 5} />
+        <div
+            style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: "3rem",
+            }}
+        >
+            <div>
+            <Link
+                to="/tracking"
+                className="btn"
+                onClick={() => {
+                window.scroll(0, 0);
+                }}
+            >
+                Track another package
+            </Link>
+            </div>
+            <div>
+            <Link
+                to="/"
+                className="btn"
+                onClick={() => {
+                window.scroll(0, 0);
+                }}
+            >
+                Go Back To Home Page
+            </Link>
+            </div>
+        </div>
+        </div>
+    </div>
+)
+
+export default TrackingDataPage;
--- Frontend/amazawn/src/pages/TrackingPage.jsx
+++ Frontend/amazawn/src/pages/TrackingPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Logo from "../components/Logo";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import ProgressBar from "../components/ProgressBar";
+import TrackingDataPage from "../components/TrackingDataPage";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styling/index.css";
@@ -14,8 +14,6 @@ import "react-toastify/dist/ReactToastify.css";
 const TrackingPage = () => {
   const [showTracking, setShowTracking] = useState(false);
   const [deliveryID, setDeliveryID] = useState("");
-
-
   const [trackingData, setTrackingData] = useState({
     progress: 0,
     eta: null,
@@ -30,7 +28,12 @@ const TrackingPage = () => {
     setDeliveryID(event.target.value);
   };
 
+  const handleCloseTracking = () => {
+    setShowTracking(false);
+  };
+
   const handleContentChange = async () => {
+    console.log("hi, entering this method");
     if (deliveryID == "") {
       console.log("Required fields are not filled");
       toast.error("Please enter a delivery ID.");
@@ -93,7 +96,7 @@ const TrackingPage = () => {
 
     } catch (error) {
       console.log('ERROR INSIDE CATCH',error);
-      toast.error('Invalid delivery ID.'); // how are you checking that the id is valid?
+      toast.error('Invalid delivery ID.'); //does not mean that ID is invalid for now, just shows that backend isnt reached
     }
   };
   
@@ -102,11 +105,10 @@ const TrackingPage = () => {
       <nav>
         <Header />
       </nav>
-      {!showTracking && (
-        <div
+      <div
           style={{ textAlign: "center", display: "block" }}
           className="trackdelivery"
-        >
+      >
           <div>
             <Logo style={{ margin: "0 auto" }} />
           </div>
@@ -131,107 +133,17 @@ const TrackingPage = () => {
             <button
               type="button"
               className="btn"
-              onClick={handleContentChange}
               style={{ marginTop: "1.5rem" }}
+              onClick={handleContentChange}
             >
               Track Delivery
             </button>
+            {showTracking && <TrackingDataPage onClick={handleCloseTracking} dyD={deliveryID} prog={trackingData.progress}  eta={trackingData.eta} progStatus={progressStatus}/>}
           </div>
-        </div>
-      )}
-
-      {showTracking && (
-        <div
-          style={{ textAlign: "center", display: "block" }}
-          className="displaytrackinfo"
-          id="track2"
-        >
-          <div>
-            <Logo style={{ margin: "0 auto" }} />
-          </div>
-          <h3 style={{ marginBottom: "3rem" }}>Track your Package</h3>
-          <div style={{ textAlign: "left" }} className="deliveryinfo">
-            <h4
-              style={{
-                marginBottom: "0.3rem",
-                marginTop: "2rem",
-                color: "#024f35",
-              }}
-            >
-              Delivery Information
-            </h4>
-
-            <div
-              style={{ display: "flex", margin: "0" }}
-              className="form-label"
-            >
-              <p style={{ fontWeight: "bold" }}> Delivery ID :&nbsp;</p>
-              <p>{deliveryID}</p>
-            </div>
-            <div
-              style={{ display: "flex", marginBottom: "0rem" }}
-              className="form-label"
-            >
-              <p style={{ fontWeight: "bold" }}> Status :&nbsp;</p>
-              <p>{progressStatus}</p>
-            </div>
-            <div
-              style={{ display: "flex", marginBottom: "0rem" }}
-              className="form-label"
-            >
-              <p style={{ fontWeight: "bold" }}>
-                Estimated Delivery Date :&nbsp;
-              </p>
-              <p>{trackingData.eta && new Date(trackingData.eta).toLocaleString()}</p>
-            </div>
-            <div
-              style={{ display: "flex", marginBottom: "0rem" }}
-              className="form-label"
-            >
-              <p style={{ fontWeight: "bold" }}>Delivery Address :&nbsp;</p>
-              <p> 1455, Blvd. De Maisonneuve Ouest, Montreal, H3G 1M8{}</p>
-            </div>
-            <div
-              style={{ display: "flex", marginBottom: "0rem" }}
-              className="form-label"
-            >
-              <p style={{ fontWeight: "bold" }}>
-                Customer support contact :&nbsp;
-              </p>
-              <p>customersupport@amazawn.com</p>
-            </div>
-
-            <ProgressBar progression={trackingData.progress / 5} />
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                marginTop: "3rem",
-              }}
-            >
-              <div>
-                <button type="button" className="btn" onClick={setShowTracking(false)}>
-                  Track another package
-                </button>
-              </div>
-              <div>
-                <Link
-                  to="/"
-                  className="btn"
-                  onClick={() => {
-                    window.scroll(0, 0);
-                  }}
-                >
-                  Go Back To Home Page
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-      <nav>
-        <Footer />
-      </nav>
+      </div>
+    <nav>
+      <Footer />
+    </nav>
     </div>
   );
 };
