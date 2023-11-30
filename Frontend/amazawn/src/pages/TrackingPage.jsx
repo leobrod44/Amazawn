6de214ed858c9ec17e2e7002053321fc17713ea4
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2023-11-30 09:38:38 -0500

    bug fix assigning var to constant. add  debug logs

--- Frontend/amazawn/src/pages/TrackingPage.jsx
+++ Frontend/amazawn/src/pages/TrackingPage.jsx
@@ -12,15 +12,20 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const TrackingPage = () => {
-  const [showTracking, setShowTracking] = useState(true);
+  const [showTracking, setShowTracking] = useState(false);
   const [deliveryID, setDeliveryID] = useState("");
+
+
   const [trackingData, setTrackingData] = useState({
     progress: 0,
     eta: null,
     lastMilestone: null
   });
-  const [progressStatus, setProgressSatus] = useState("");
-  
+
+ // const [progressStatus, setProgressSatus] = useState("");
+ //cannot be constant or else you cant reassign it inside ur try
+  let progressStatus='';
+
   const handleInputChange = (event) => {
     setDeliveryID(event.target.value);
   };
@@ -31,9 +36,8 @@ const TrackingPage = () => {
       toast.error("Please enter a delivery ID.");
       return;
     }
-    setShowTracking(false);
+
     try {
-      //get the current date
       const currentDate = new Date();
       currentDate.setDate(currentDate.getDate() + 1);
       const formattedCurrentDate = currentDate.toISOString();
@@ -42,8 +46,8 @@ const TrackingPage = () => {
         shipmentID: deliveryID,
         currentDate: formattedCurrentDate,
       };
-      console.log(deliveryID);
-      console.log(requestData);
+      console.log('DELIVERY ID INSIDE THE TRY',deliveryID);
+      console.log('REQUEST DATA INSIDE THE TRY',requestData);
 
       const response = await axios.post(
         "http://localhost:8080/tracking/trackShipment",
@@ -53,28 +57,43 @@ const TrackingPage = () => {
             "Content-Type": "application/json",
           },
         }
-      ).then((response) => setTrackingData(response.data));
-      console.log(response.data);
+      );
+      console.log('THIS IS THE BACKEND RESPONSE DATA',response.data);
+
+      console.log('PROGESS',response.data.progress)
+      console.log('TRACKING DATA', trackingData);
 
       if (response.data.progress == 1) {
         progressStatus = "order creater at " + trackingData.lastMilestone && new Date(trackingData.lastMilestone).toLocaleString();
+      
       } else if (response.data.progress == 2) {
         progressStatus = "package picked up " + trackingData.lastMilestone && new Date(trackingData.lastMilestone).toLocaleString();
+     
       } else if (response.data.progress == 3) {
         progressStatus = "package arrived at center 1 " + trackingData.lastMilestone && new Date(trackingData.lastMilestone).toLocaleString();
+      
       } else if (response.data.progress == 4) {
         progressStatus = "package arrived at center 2 " + trackingData.lastMilestone && new Date(trackingData.lastMilestone).toLocaleString();
+      
       } else if (response.data.progress == 5) {
         progressStatus = "arrived at destination " + trackingData.lastMilestone && new Date(trackingData.lastMilestone).toLocaleString();
+     
       } else {
         progressStatus = "No update yet";
       }
+
+      console.log("MADE IT PASSED THE TRY");
+      console.log(progressStatus);
       
+      ///testing to see if setshowtracking works, it doesnt. it stayts false even after setting it  to true
+      console.log("showTracking before:", showTracking);
+      setShowTracking(true);
+      console.log("showTracking after:", showTracking);
+     
+
     } catch (error) {
-      console.log(error);
-      // Handle errors if the request fails
-      toast.error('Invalid delivery ID.');
-      setShowTracking(false);
+      console.log('ERROR INSIDE CATCH',error);
+      toast.error('Invalid delivery ID.'); // how are you checking that the id is valid?
     }
   };
   
@@ -83,7 +102,7 @@ const TrackingPage = () => {
       <nav>
         <Header />
       </nav>
-      {(showTracking==false) && (
+      {!showTracking && (
         <div
           style={{ textAlign: "center", display: "block" }}
           className="trackdelivery"
@@ -121,7 +140,7 @@ const TrackingPage = () => {
         </div>
       )}
 
-      {(showTracking==true) && (
+      {showTracking && (
         <div
           style={{ textAlign: "center", display: "block" }}
           className="displaytrackinfo"
